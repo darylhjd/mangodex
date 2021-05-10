a6a9a84ac7b26dcea5f787f71bac5f4ef6946d85
Daryl <aka.hjd@gmail.com>
2021-05-10 23:19:31 +0800

    New methods to help with getting chapter page data.

--- at_home.go
+++ at_home.go
@@ -1,25 +1,47 @@
 package mangodex
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strconv"
+	"strings"
+	"time"
 )
 
 const (
 	GetMDHomeURLPath = "at-home/server/%s"
+	MDHomeReportURL  = "https://api.mangadex.network/report"
 )
 
-// GetMDHomeURL : Get MangaDex@Home URL.
-// https://api.mangadex.org/docs.html#operation/get-at-home-server-chapterId
-func (dc *DexClient) GetMDHomeURL(chapId string, forcePort443 bool) (string, error) {
-	return dc.GetMDHomeURLContext(context.Background(), chapId, forcePort443)
+type MDHomeClient struct {
+	Client    http.Client
+	BaseURL   string
+	Quality   string
+	Hash      string
+	ChapterID string
 }
 
-// GetMDHomeURLContext : GetMDHomeURL with custom context.
-func (dc *DexClient) GetMDHomeURLContext(ctx context.Context, chapId string, forcePort443 bool) (string, error) {
+type ReportPayload struct {
+	URL      string
+	Success  bool
+	Cached   bool
+	Bytes    int
+	Duration int64
+}
+
+// NewMDHomeClient : Get MangaDex@Home client for a chapter.
+// https://api.mangadex.org/docs.html#operation/get-at-home-server-chapterId
+func (dc *DexClient) NewMDHomeClient(chapId, quality, hash string, forcePort443 bool) (*MDHomeClient, error) {
+	return dc.NewMDHomeClientContext(context.Background(), chapId, quality, hash, forcePort443)
+}
+
+// NewMDHomeClientContext : NewMDHomeClient with custom context.
+func (dc *DexClient) NewMDHomeClientContext(ctx context.Context, chapId, quality, hash string, forcePort443 bool) (*MDHomeClient, error) {
 	u, _ := url.Parse(BaseAPI)
 	u.Path = fmt.Sprintf(GetMDHomeURLPath, chapId)
 
@@ -30,5 +52,73 @@ func (dc *DexClient) GetMDHomeURLContext(ctx context.Context, chapId string, for
 
 	var r map[string]string
 	_, err := dc.RequestAndDecode(ctx, http.MethodGet, u.String(), nil, &r)
-	return r["baseUrl"], err
+	if err != nil {
+		return nil, err
+	}
+
+	return &MDHomeClient{
+		Client:    http.Client{},
+		BaseURL:   r["baseUrl"],
+		Quality:   quality,
+		Hash:      hash,
+		ChapterID: chapId,
+	}, nil
+}
+
+// Report : Report success of getting chapter page data.
+func (c *MDHomeClient) Report(r ReportPayload) (*http.Response, error) {
+	return c.ReportContext(context.Background(), r)
+}
+
+// ReportContext : Report with custom context.
+func (c *MDHomeClient) ReportContext(ctx context.Context, r ReportPayload) (*http.Response, error) {
+	rBytes, err := json.Marshal(&r)
+	if err != nil {
+		return nil, err
+	}
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, MDHomeReportURL, bytes.NewBuffer(rBytes))
+	if err != nil {
+		return nil, err
+	}
+	return c.Client.Do(req)
+}
+
+// GetChapterPage : Return page data for a chapter with the filename of that page.
+func (c *MDHomeClient) GetChapterPage(filename string) ([]byte, error) {
+	return c.GetChapterPageWithContext(context.Background(), filename)
+}
+
+// GetChapterPageWithContext : GetChapterPage with custom context.
+func (c *MDHomeClient) GetChapterPageWithContext(ctx context.Context, filename string) ([]byte, error) {
+	u, _ := url.Parse(c.BaseURL)
+	u.Path = strings.Join([]string{c.Quality, c.Hash, c.ChapterID, filename}, "/")
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	r := ReportPayload{
+		URL:      u.String(),
+		Success:  true,
+		Cached:   false,
+		Bytes:    0,
+		Duration: 0,
+	}
+
+	start := time.Now()
+	resp, err := c.Client.Do(req)
+	if err != nil {
+		r.Success = false
+		r.Duration = time.Since(start).Milliseconds()
+		_, _ = c.ReportContext(ctx, r) // Make report
+		return nil, err
+	}
+
+	b, err := ioutil.ReadAll(resp.Body)
+	r.Duration = time.Since(start).Milliseconds()
+	r.Bytes = len(b)
+	r.Cached = resp.Header.Get("X-Cache") == "HIT"
+	_, _ = c.ReportContext(ctx, r) // Make report
+	return b, err
 }
